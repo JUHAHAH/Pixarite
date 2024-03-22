03bde0d28dcd84d30bc7ec8e4bc9117c07532e08
JUHAHAH <juha0227@gmail.com>
2024-03-22 13:37:08 +0900

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,12 +1,3 @@
 import prisma from '@/lib/database/prisma';
-import { useUser } from '@clerk/nextjs';
 
-const clerkUser = useUser().user;
-
-export async function GET() {
-  const getUser = await prisma.user.findUnique({
-    where: { externalId: clerkUser?.id },
-  });
-
-  return Response.json({ getUser });
-}
+export async function GET() {}
